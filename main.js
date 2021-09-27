12bfa71913fe6e5a3834aa3d68a36c32b6e4c45e
yatsinoa <sashayatzino@gmail.com>
2021-09-27 20:26:20 +0300

    new

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 import './css/style.scss';
 import './js/index.js';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
