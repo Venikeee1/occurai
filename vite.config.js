9024b2cb5fdfac2750fe211b1e57055077ab22f8
oleksandr_vienik <alexandrVenik@gmail.com>
2021-11-06 15:31:21 +0200

    add multiple page feature

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import StylelintPlugin from '@frsource/vite-plugin-stylelint';
+
+const root = resolve(__dirname, 'src');
 
 export default defineConfig({
   root: 'src',
   base: '/occurai/',
   plugins: [StylelintPlugin()],
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        companies: resolve(root, 'companies.html'),
+      },
+    },
+  },
 });
